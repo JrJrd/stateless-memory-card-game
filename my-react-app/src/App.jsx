8bd8f252d1bfa69b1f83993e208af491690944fb
seaer <jrd.seaer@gmail.com>
2023-09-24 00:12:03 +0300

    setTimeout

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
+import { useEffect } from "react";
 
 const cardImages = [
-  { src: "/img/1.png" },
-  { src: "/img/2.png" },
-  { src: "/img/3.png" },
-  { src: "/img/4.png" },
-  { src: "/img/5.png" },
-  { src: "/img/6.png" },
+  { src: "/img/1.png", matched: false },
+  { src: "/img/2.png", matched: false },
+  { src: "/img/3.png", matched: false },
+  { src: "/img/4.png", matched: false },
+  { src: "/img/5.png", matched: false },
+  { src: "/img/6.png", matched: false },
 ];
 
 function App() {
@@ -25,21 +26,56 @@ function App() {
 
     setCards(shuffledCards);
     setTurns(0);
+    setChoiceOne(null);
+    setChoiceTwo(null);
   };
 
   const handleChoice = (card) => {
     console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
-  console.log(cards, turns);
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+       setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+  //console.log(cards, turns);
+  useEffect(() => {
+    shuffleCards()
+  }, [])
   return (
     <div className="App">
-      <h1>Game</h1>
-      <button onClick={shuffleCards}>New</button>
+      <h1>Start</h1>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
-        ))}
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice}
+          flipped={card === choiceOne || card === choiceTwo || card.matched} />
+          ))}
       </div>
+      <p>Turns: {turns}</p>
+          <button onClick={shuffleCards}>Shuffle</button>
     </div>
   );
 }
